31146d4998be15f9e2d8ec2c8c9738e3d6763481
jorge.jimenez <jorge.jimenez@dinamicarea.com>
2016-02-11 20:57:41 +0100

    feat contructores de los servicios

--- src/main/java/es/opileak/hashcode/services/WarehouseService.java
+++ src/main/java/es/opileak/hashcode/services/WarehouseService.java
@@ -1,8 +1,16 @@
 package es.opileak.hashcode.services;
 
+import java.util.List;
+
 import es.opileak.hashcode.model.Candidate;
+import es.opileak.hashcode.model.Warehouse;
 
 public class WarehouseService {
+
+	public WarehouseService(List<Warehouse> warehouseList) {
+		super();
+
+	}
 
 	public void update(Candidate operationCandidate) {
 		// TODO Auto-generated method stub
