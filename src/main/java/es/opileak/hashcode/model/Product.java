836135892d3ee58964d85947eed9c226869b34a6
Alberto Trillo <alberto.trillo@dinamicarea.com>
2016-02-11 20:32:46 +0100

    feat: clase producto

--- src/main/java/es/opileak/hashcode/model/Product.java
+++ src/main/java/es/opileak/hashcode/model/Product.java
@@ -2,4 +2,53 @@ package es.opileak.hashcode.model;
 
 public class Product {
 
+	Integer	id;
+	Integer	peso;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Integer peso) {
+		this.peso = peso;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + "]";
+	}
+
 }
