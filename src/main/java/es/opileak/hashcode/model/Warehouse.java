eab3440f5a7c78f8543237af8f673a8a2e25480d
Ignacio Domingo Martínez <i.domingo@dinamicarea.com>
2016-02-11 20:29:53 +0100

    warehouse

--- src/main/java/es/opileak/hashcode/model/Warehouse.java
+++ src/main/java/es/opileak/hashcode/model/Warehouse.java
@@ -1,5 +1,25 @@
 package es.opileak.hashcode.model;
 
-public class Warehouse {
+import java.awt.geom.Point2D;
+import java.util.Map;
 
+public class Warehouse {
+	private Point2D					position;
+	private Map<Product, Integer>	productsQuantity;
+
+	public Point2D getPosition() {
+		return position;
+	}
+
+	public void setPosition(Point2D position) {
+		this.position = position;
+	}
+
+	public Map<Product, Integer> getProductsQuantity() {
+		return productsQuantity;
+	}
+
+	public void setProductsQuantity(Map<Product, Integer> productsQuantity) {
+		this.productsQuantity = productsQuantity;
+	}
 }
