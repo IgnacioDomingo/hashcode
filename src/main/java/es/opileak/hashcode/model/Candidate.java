de9cd87aeeb78be78b2278e4e3aade0040e0246f
Alberto Trillo <alberto.trillo@dinamicarea.com>
2016-02-11 20:25:28 +0100

    feat: beans Candidate, command, drone

--- src/main/java/es/opileak/hashcode/model/Candidate.java
+++ src/main/java/es/opileak/hashcode/model/Candidate.java
@@ -1,5 +1,35 @@
 package es.opileak.hashcode.model;
 
+import java.util.List;
+
 public class Candidate {
+
+	List<Command>	lstCommands;
+	Integer			turns;
+	Integer			score;
+
+	public List<Command> getLstCommands() {
+		return lstCommands;
+	}
+
+	public void setLstCommands(List<Command> lstCommands) {
+		this.lstCommands = lstCommands;
+	}
+
+	public Integer getTurns() {
+		return turns;
+	}
+
+	public void setTurns(Integer turns) {
+		this.turns = turns;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
 
 }
--- src/main/java/es/opileak/hashcode/model/Command.java
+++ src/main/java/es/opileak/hashcode/model/Command.java
@@ -1,5 +1,36 @@
 package es.opileak.hashcode.model;
 
+import java.awt.geom.Point2D;
+import java.util.List;
+
 public class Command {
+
+	Point2D			posicion;
+	Order			orden;
+	List<Command>	lstCommand;
+
+	public Point2D getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Point2D posicion) {
+		this.posicion = posicion;
+	}
+
+	public Order getOrden() {
+		return orden;
+	}
+
+	public void setOrden(Order orden) {
+		this.orden = orden;
+	}
+
+	public List<Command> getLstCommand() {
+		return lstCommand;
+	}
+
+	public void setLstCommand(List<Command> lstCommand) {
+		this.lstCommand = lstCommand;
+	}
 
 }
